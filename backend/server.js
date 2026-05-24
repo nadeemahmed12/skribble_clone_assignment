136b5b9a9c20f67048ff4f800f1c26dd5b9e90f1
nadeemahmed12 <nadeemahmed9480@gmail.com>
2026-05-24 10:44:18 +0530

    fixed vercel cors

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
-      "https://skribble-clone-assignment.vercel.app",
-      "https://skribble-clone-bf2ci7t8f.vercel.app",
+      "https://skribble-clone-assignment.vercel.app/"
     ],
     methods: ["GET", "POST"],
   },
