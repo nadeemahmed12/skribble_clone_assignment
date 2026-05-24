45a780e6cca23120fff58c594f86050dd23308a8
nadeemahmed12 <nadeemahmed9480@gmail.com>
2026-05-24 10:23:46 +0530

    added production urls

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://skribble-clone-assignment.vercel.app",
+    ],
     methods: ["GET", "POST"],
   },
 });
